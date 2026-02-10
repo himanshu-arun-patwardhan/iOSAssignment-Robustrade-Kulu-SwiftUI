c0e305d2e90047f2864174bd78b33fbd1d1f6084
Himanshu Arun Patwardhan <himanshu.arun.patwardhan@gmail.com>
2026-02-10 17:37:02 +0530

    [ViewModel] - product view model

---
+++ iOSAssignment-Robustrade-Kulu-SwiftUI/iOSAssignment-Robustrade-Kulu-SwiftUI/Product/Screen/ProductListViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  ProductListViewModel.swift
+//  iOSAssignment-Robustrade-Kulu-SwiftUI
+//
+//  Created by Himanshu Patwardhan on 10/02/26.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+class ProductListViewModel: ObservableObject {
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String? = nil
+    @Published var products: [Product] = []
+    
+    private var page = 0
+    private var total = 0
+    
+    private let limit = 10
+    private let category = ""
+    
+    var hasMorePges: Bool {
+        products.count < total
+    }
+    
+    
+    let apiService: APIService
+    
+    init(apiService: APIService) {
+        self.apiService = apiService
+    }
+    
+    
+    private func reset() {
+        page =  0
+        total = 0
+        products = []
+    }
+    
+    func loadInitial() async {
+        reset()
+        await loadData()
+    }
+    
+    func loadData() async {
+        guard !isLoading, hasMorePges || page == 0 else {
+            return
+        }
+        
+        isLoading = true
+        errorMessage  = nil
+        
+        do {
+            let response = try await apiService.fetchProducts(page: page,
+                                                              limit: limit,
+                                                              category: category)
+            total = response.pagination.total
+            page = page + 1
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+        
+        isLoading = false
+    }
+}
