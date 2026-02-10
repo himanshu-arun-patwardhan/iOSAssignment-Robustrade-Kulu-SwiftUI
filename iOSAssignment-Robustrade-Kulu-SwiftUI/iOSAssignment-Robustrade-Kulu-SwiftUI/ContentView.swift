23c399d9098c337c70a55c49c575884bc6181829
Himanshu Arun Patwardhan <himanshu.arun.patwardhan@gmail.com>
2026-02-10 16:36:03 +0530

    Xcode new project -  App - SwiftUI - basic initial setup

---
+++ iOSAssignment-Robustrade-Kulu-SwiftUI/iOSAssignment-Robustrade-Kulu-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSAssignment-Robustrade-Kulu-SwiftUI
+//
+//  Created by Himanshu Patwardhan on 10/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSAssignment-Robustrade-Kulu-SwiftUI/iOSAssignment-Robustrade-Kulu-SwiftUI/iOSAssignment_Robustrade_Kulu_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAssignment_Robustrade_Kulu_SwiftUIApp.swift
+//  iOSAssignment-Robustrade-Kulu-SwiftUI
+//
+//  Created by Himanshu Patwardhan on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSAssignment_Robustrade_Kulu_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
